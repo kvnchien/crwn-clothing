4777c93f70c00cf58e5d588edcf09dc854108ba7
Kevin Chien <kchien@Kevins-Mac-Pro.home>
2021-12-17 22:45:59 -0600

    Add a Header component and styles

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 //import AnotherPage from './test/anotherpage.component';
 
 
@@ -22,6 +23,7 @@ function App() {
       {/* The following <Switch/> and <Route/> are react-router-dom 5.x feature
           The react-router-dom 6 uses <Routers/> in place of <Switch/> 
       */}
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
