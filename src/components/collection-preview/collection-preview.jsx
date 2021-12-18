cc53f98e5bb34084629b7b103fe3bbf131b09641
Kevin Chien <kchien@Kevins-Mac-Pro.home>
2021-12-17 19:23:04 -0600

    updated

--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -11,8 +11,7 @@ const CollectionPreview = ({ title, items }) => (
       {items
         .filter((item, idx) => idx < 4)
         .map(({ id, ...otherItemProps }) => (
-          // <CollectionItem key={id} {...otherItemProps} />
-          <div>name</div>
+          <CollectionItem key={id} {...otherItemProps} />
         ))}
     </div>
   </div>
