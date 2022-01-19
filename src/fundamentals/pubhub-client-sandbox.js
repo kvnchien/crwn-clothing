ee7a6e6b5503e882a04fce4b6b21887fd103f3cb
Kevin Chien <kevin_chien@omnisys.com>
2022-01-19 13:50:43 -0600

    Added pubhub implementation

--- src/fundamentals/pubhub-client-sandbox.js
+++ src/fundamentals/pubhub-client-sandbox.js
@@ -1,29 +1,96 @@
 const fetch = require('node-fetch');
+const apiTokenUrl = 'https://identity.nuvolahubapi.io/oauth2/token'
+const ibUrlBase = 'https://sandbox-110.nuvolahub.com:8443';
 
-const headers = () => {
-  let client_cred = "1971otu0j3n5g6omvegg7lschj:2n7bbee090sp0eebf1eoe594btvomeprkf5dau4gpnl4iiovbkj"
-  let client_cred_64 = Buffer.from(client_cred, 'binary').toString('base64')
+/**
+ * Authenticate with 
+ * @returns Token response object
+ */
+const apiAuth = async () => {
+    var client_cred = "1971otu0j3n5g6omvegg7lschj:2n7bbee090sp0eebf1eoe594btvomeprkf5dau4gpnl4iiovbkj"
+    var client_cred_64 = Buffer.from(client_cred, 'binary').toString('base64')
 
-  return {"Content-Type":"application/x-www-form-urlencoded", 
-          "Authorization":"Basic "+client_cred_64};
-}
-
-const dispatch = (token) => {
-    console.log(token);
-}
-
-const getApiToken = () => {
-    fetch('https://identity.nuvolahubapi.io/oauth2/token', {
+    var response = await fetch(apiTokenUrl, {
             method: 'POST', 
-            headers: headers(),
-            body: "grant_type=client_credentials"})
-        .then(response => response.json())
-        .then(data => {
-            dispatch(data.access_token);
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
+            headers: {"Content-Type":"application/x-www-form-urlencoded", 
+                      "Authorization":"Basic "+client_cred_64},
+            body: "grant_type=client_credentials"});
+
+    return response.json();
 };
 
-export default getApiToken;
+/**
+ * Build the request headers for IB API queries
+ * @param {*} token 
+ * @returns Request headers
+ */
+const reqHeaders = (token) => {
+    return {
+        "Authorization": "Bearer "+token.access_token,
+        "Content-Type":"application/json"}
+}
+
+
+const IBLocationFetch = (endpoint, data) => {
+    apiAuth().then(
+        token => {   
+            fetch(endpoint, {
+                method: 'GET', 
+                headers: reqHeaders(token),
+                body: data})
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                });
+        }
+    )
+}
+
+const IBPatientQuery = (endpoint, data) => {
+    apiAuth().then(
+        token => {   
+            fetch(endpoint, {
+                method: 'POST', 
+                headers: reqHeaders(token),
+                body: JSON.stringify(data)})
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                });
+        }
+    )
+}
+
+
+//Query IB's location data
+IBLocationFetch(ibUrlBase+'/api/BIyPiqm6xYjc6M6ebOy1b/ph/locations/18', {});
+
+
+//Query state registry for patient vaccine history/forecast
+var patientQueryData = {
+    "bridgeway_id": 32,
+    "data": {
+      "mrn": "111111111111",
+      "last_name": "Plastname1",
+      "first_name": "Pfirstname1",
+      "middle_initial": "",
+      "dob": "05/15/1950",
+      "gender": "M",
+      "address": "11111 Dallas Noway",
+      "city": "Dallas",
+      "state": "TX",
+      "zip": "75252",
+      "phone": {
+        "area_code": "972",
+        "local_number": "321-0000"
+      },
+      "last_update_date": "01/01/2011",
+      "last_update_facility": "Test Facility CA3",
+      "multiple_birth_indicator": "N",
+      "birth_order": 1,
+      "mother_last_name": "last",
+      "mother_first_name": "first"
+    },
+    "environment": "Testing"
+  }
+IBPatientQuery(ibUrlBase+'/api/frVOVQcerOKM4IYWEkvYx/ph/registries/60/query', patientQueryData)
--- src/fundamentals/tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-const myArray = [1,2,3,4,5]
-
-const result = myArray.reduce((accumulator, currentElement) => 
-accumulator + currentElement, 1);
-
-console.log(result);
