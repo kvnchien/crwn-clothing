9f4ef14dd6753eae80ad3d22a90c7d21bc95884b
Kevin Chien <kchien@Kevins-Mac-Pro.home>
2022-01-25 23:25:24 -0600

    Added a checkout page and updated dart-dropdown to use withRouter function

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,16 +6,22 @@ import CartItem from '../cart-item/cart-item.component';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 import { createStructuredSelector } from 'reselect';
 
+import { withRouter } from 'react-router-dom';
+
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({cartItems}) => (
+const CartDropdown = ({cartItems, history}) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
         {
+            cartItems.length ?
             cartItems.map(cartItem => ( <CartItem key={cartItem.id} item={cartItem} />
-        ))}
+        ))
+            :
+            <span className='empty-message'>Your cart is empty</span>    
+        }
         </div>
-        <CustomButton>GO TO CHECKOUT</CustomButton>
+        <CustomButton onClick={() => history.push('./checkout')}>GO TO CHECKOUT</CustomButton>
     </div>
 )
 
@@ -36,5 +42,11 @@ const mapStateToProps = createStructuredSelector ({
 //     cartItems
 // })
 
-
-export default connect(mapStateToProps)(CartDropdown);
+//The "withRouter" is not longer valid in react-reouter-dom 6.x. Use hook
+//The "withRouter" will return the same MeunItem component and add the 'props' coming 
+//from the <Route></Route>. So the "history, linkUrl, match" props are passed
+//to the MenuItem by the "withRouter" function.. 
+//
+//See ./components/menu-item/menu-item.component.jsx for what props are made available 
+//with the 'withRouter' function
+export default withRouter(connect(mapStateToProps)(CartDropdown));
