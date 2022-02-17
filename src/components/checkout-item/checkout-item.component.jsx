e36af298cd55eb63a0785dbba20b53cd43fc9478
Kevin Chien <kchien@Kevins-Mac-Pro.home>
2022-02-16 23:36:30 -0600

    Implemented add/remove cart items

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import { clearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.actions';
+
 import './checkout-item.styles.scss';
 
 
@@ -26,23 +28,53 @@ import './checkout-item.styles.scss';
 //b. The 'cartItem' object is a dictionary containing the imageUrl, price, name, quantity information.. 
 //   These values are destructured from the props along with the 'cartItem' object itself.
 //   See above code block without using the desructuring construct... 
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity}}) => (
-    <div className='checkout-item'>
-        <div className='image-container'>
-            <img alt='item' src={imageUrl}/>
+
+// const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity}}) => (
+//     <div className='checkout-item'>
+//         <div className='image-container'>
+//             <img alt='item' src={imageUrl}/>
+//         </div>
+//         <span className='name'>{name}</span>
+//         <span className='quantity'>{quantity}</span>
+//         <span className='price'>{price}</span>
+
+//         {/*  https://www.w3schools.com/charsets/ref_utf_dingbats.asp */}
+//         <div className='remove-button'>&#10005;</div>
+//     </div>
+// );
+
+//See notes above first.
+//The destructuring construct above { cartItem: { name, imageUrl, price, quantity}} won't 
+//give us access to the 'cartItem' object (i.e. You can't do cartItem.name or cartItem.imageUrl to reference the properties)
+//We have to do it separately. And the addItem and removeItem are props provided by the mapDispatchToProps function
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem}) => {
+    //Destructure the 'cartItem' here:
+    const { name, imageUrl, price, quantity } = cartItem;
+
+    return (
+        <div className='checkout-item'>
+            <div className='image-container'>
+                <img alt='item' src={cartItem.imageUrl}/>
+            </div>
+            <span className='name'>{name}</span>
+            <span className='quantity'>
+                <div className='arrow' onClick={() => removeItem(cartItem)}>&#10094;</div>
+                <span className='value'>{quantity}</span>
+                <div className='arrow' onClick={() => addItem(cartItem)}>&#10095;</div>
+            </span>
+            <span className='price'>{price}</span>
+
+            {/*  https://www.w3schools.com/charsets/ref_utf_dingbats.asp */}
+            <div className='remove-button' onClick={() => clearItem(cartItem)}>&#10005;</div>
         </div>
-        <span className='name'>{name}</span>
-        <span className='quantity'>{quantity}</span>
-        <span className='price'>{price}</span>
+    );
+};
 
-        {/*  https://www.w3schools.com/charsets/ref_utf_dingbats.asp */}
-        <div className='remove-button'>&#10005;</div>
-    </div>
-);
-
-const mapStateToProps = () => {
-
-}
+const mapDispatchToProps = dispatch => ({
+    clearItem: item => dispatch(clearItemFromCart(item)), 
+    addItem: item => dispatch(addItem(item)),
+    removeItem: item => dispatch(removeItem(item))
+})
 
 
-export default connect()(CheckoutItem);
+export default connect(null, mapDispatchToProps)(CheckoutItem);
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,17 +1,37 @@
  export const addItemToCart = (cartItems, cartItemToAdd) => {
-    
-    const existingCartItem = cartItems.find(
-      cartItem => cartItem.id === cartItemToAdd.id
+
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToAdd.id
+  );
+  
+  if (existingCartItem) {
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
     );
+  }
   
-    if (existingCartItem) {
-      return cartItems.map(cartItem =>
-        cartItem.id === cartItemToAdd.id
-          ? { ...cartItem, quantity: cartItem.quantity + 1 }
-          : cartItem
-      );
-    }
-  
-    return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
     
-  };
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+
+  //Get the cartItem that's marked for removal from the cartItems
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  )
+
+  //If the existingCartItem quantity is 1, we just remove the entire cartItem
+  if(existingCartItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+  }
+
+  //So the existingCartItem has > 1 quantity, we need to reduce the count... 
+  return cartItems.map(cartItem =>
+    cartItem.id === cartItemToRemove.id 
+      ? { ...cartItem, quantity: cartItem.quantity -1 }
+      : cartItem //Can we just ignore this line? becasue nothing to do to reduce the quantity count anyway.. 
+  );
+};
